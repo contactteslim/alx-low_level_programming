c29292924afe612f740a7f674fa190544d7de576
Opeyemi Ogunsina <contactteslim@gmail.com>
2023-03-17 15:48:50 -0700

    Program that prints digits in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints numbers in base 16.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
