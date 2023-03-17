67ad0957d3e312ae67134f6a9ad95b9c71779374
Opeyemi Ogunsina <contactteslim@gmail.com>
2023-03-17 15:13:36 -0700

    A program that prints numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints number in base 10 starting from 0.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
