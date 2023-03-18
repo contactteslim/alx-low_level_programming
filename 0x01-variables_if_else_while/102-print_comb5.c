fd187c6c2876ac4763e8a1e74c820ff8bf1e28b8
Opeyemi Ogunsina <contactteslim@gmail.com>
2023-03-18 06:21:46 -0700

    Program that prints all possible double digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints double numbers.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i + 0; i < 100; i++)
+	{
+		for (j + 0; j < 100; j++)
+		{
+			int i1 = i / 10;
+			int i2 = i % 10;
+			int j1 = j / 10;
+			int j2 = j % 10;
+
+			if (i < j)
+			{
+				putchar(i1 + '0');
+				putchar(i2 + '0');
+				putchar(' ');
+				putchar(j1 + '0');
+				putchar(j2 + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' '};
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
