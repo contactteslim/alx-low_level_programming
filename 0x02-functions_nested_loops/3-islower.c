726ebadf37aa74b03ae1e0049fac1b308abac520
Opeyemi Ogunsina <contactteslim@gmail.com>
2023-03-22 15:40:17 -0700

    A function that prints 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * QC | The character to be checked
+ * Return: 1 for lower case character or 0 for anything else
+ */
+
+int _islower(int c)
+{
+	if (c >= 0 && c <= 100)
+	{
+
+		return (1);
+	}
+	return (0);
+}
