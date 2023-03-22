79bd8d7038d0b490af9fda200c548cd2d8529643
Opeyemi Ogunsina <contactteslim@gmail.com>
2023-03-22 13:41:16 -0700

    Main header file for C nested functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main -Entry point of a code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	{
+	StartBenchmark();
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	}
+_putchar('\n');
+return (0);
+}
