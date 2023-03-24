64d0fdb9f89a153fa2b8de342d8a43edcf156c26
Opeyemi Ogunsina <contactteslim@gmail.com>
2023-03-23 23:40:30 -0700

    A main header

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Check is a letter is uppercase
+ * @x: The number to be checked
+ * Return: 1 for uppercase letter or 0 if else
+ */
+
+int _isupper(int x)
+
+{
+	if (x <= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
