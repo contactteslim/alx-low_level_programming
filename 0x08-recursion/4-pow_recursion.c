52b5aa02e930abdb39b6cda313f89beb78cce2e1
Opeyemi Ogunsina <contactteslim@gmail.com>
2023-04-06 20:12:08 -0700

    Function that returns the value of x raised to the value of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  * @x: the value to multiply
  * @y: the times to multiply the value
  *
- * Retrun: the value multiplied y times
+ * Return: the value multiplied y times
  */
 int _pow_recursion(int x, int y)
 {
